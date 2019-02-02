493843ec32d619d9baa4f30fc8d13a34aced4122
Dylan-N-Rem <32395209+Dylan-N-Rem@users.noreply.github.com>
2019-02-03 02:55:44 +0800

    Create rolls.py

---
+++ rolls.py
@@ -0,0 +1,599 @@
+def roll0():
+    roll0 = []
+    for u01 in range(23):
+        roll0 += ["Crab Long Bao"]
+        roll0 += ["Gingerbread"]
+    for u02 in range(61):
+        roll0 += ["Bamboo Rice"]
+        roll0 += ["Peking Duck"]
+        roll0 += ["B-52"]
+    for u03 in range(62):
+        roll0 += ["Foie Gras"]
+    for uf4 in range(5):
+        roll0 += ["Boston Lobster"]
+        roll0 += ["Double Scoop"]
+    for s01 in range(103):
+        roll0 += ["Tiramisu"]
+        roll0 += ["Escargot"]
+        roll0 += ["Hotdog"]
+    for s02 in range(104):
+        roll0 += ["Mango Pudding"]
+        roll0 += ["Hamburger",]
+        roll0 += ["Steak"]
+        roll0 += ["Tangyuan"]
+        roll0 += ["Sanma"]
+        roll0 += ["Napoleon Cake"]
+        roll0 += ["Salad"]
+        roll0 += ["Pastel de nata"]
+        roll0 += ["Yuxiang"]
+        roll0 += ["Sukiyaki"]
+        roll0 += ["Brownie"]
+        roll0 += ["Red Wine"]
+        roll0 += ["Gyoza"]
+    for r01 in range(413):
+        roll0 += ["Long Bao"]
+        roll0 += ["Coffee"]
+        roll0 += ["Sashimi"]
+        roll0 += ["Macaron"]
+        roll0 += ["Zongzi"]
+        roll0 += ["Sakuramochi"]
+        roll0 += ["Tom Yum"]
+        roll0 += ["Taiyaki"]
+        roll0 += ["Milk"]
+        roll0 += ["Dorayaki"]
+        roll0 += ["Sake"]
+        roll0 += ["Tempura"]
+        roll0 += ["Spicy Gluten"]
+    for r02 in range(414):
+        roll0 += ["Jiuniang"]
+        roll0 += ["Omurice"]
+        roll0 += ["Orange Juice"]
+        roll0 += ["Ume Ochazuke" ,]
+        roll0 += ["Miso Soup" ,]
+        roll0 += ["Yellow Wine"]
+    for m01 in range(62):
+        roll0 += ["Pancake"]
+        roll0 += ["Jello"]
+    for m02 in range(61):
+        roll0 += ["Skewer"]
+    return roll0
+
+def roll1():
+    roll1 = []
+    for u11 in range(23):
+        roll1 += ["Crab Long Bao"]
+        roll1 += ["Gingerbread"]
+    for u12 in range(61):
+        roll1 += ["Foie Gras"]
+        roll1 += ["Peking Duck"]
+        roll1 += ["B-52"]
+    for u13 in range(62):
+        roll1 += ["Bamboo Rice"]
+    for u14 in range(5):
+        roll1 += ["Boston Lobster"]
+        roll1 += ["Double Scoop"]
+    for s11 in range(83):
+        roll1 += ["Tiramisu"]
+        roll1 += ["Escargot"]
+        roll1 += ["Hotdog"]
+        roll1 += ["Mango Pudding"]
+        roll1 += ["Hamburger",]
+        roll1 += ["Steak"]
+        roll1 += ["Tangyuan"]
+        roll1 += ["Sanma"]
+        roll1 += ["Napoleon Cake"]
+        roll1 += ["Salad"]
+        roll1 += ["Pastel de nata"]
+        roll1 += ["Yuxiang"]
+        roll1 += ["Sukiyaki"]
+        roll1 += ["Brownie"]
+        roll1 += ["Red Wine"]
+        roll1 += ["Gyoza"]
+        roll1 += ["Chocolate"]
+    for r11 in range(413):
+        roll1 += ["Long Bao"]
+        roll1 += ["Coffee"]
+        roll1 += ["Sashimi"]
+        roll1 += ["Macaron"]
+        roll1 += ["Zongzi"]
+        roll1 += ["Sakuramochi"]
+        roll1 += ["Tom Yum"]
+        roll1 += ["Taiyaki"]
+        roll1 += ["Milk"]
+        roll1 += ["Dorayaki"]
+        roll1 += ["Sake"]
+        roll1 += ["Tempura"]
+        roll1 += ["Spicy Gluten"]
+    for r12 in range(414):
+        roll1 += ["Jiuniang"]
+        roll1 += ["Omurice"]
+        roll1 += ["Orange Juice"]
+        roll1 += ["Ume Ochazuke" ,]
+        roll1 += ["Miso Soup" ,]
+        roll1 += ["Yellow Wine"]
+    for m11 in range(46):
+        roll1 += ["Skewer"]
+        roll1 += ["Jello"]
+        roll1 += ["Pancake"]
+    for m12 in range(47):
+        roll1 += ["Popcorn"]
+    return roll1
+
+def roll2():
+    roll2 = []
+    for u21 in range(23):
+        roll2 += ["Crab Long Bao"]
+        roll2 += ["Gingerbread"]
+    for u22 in range(61):
+        roll2 += ["Foie Gras"]
+        roll2 += ["Peking Duck"]
+        roll2 += ["B-52"]
+    for u23 in range(62):
+        roll2 += ["Bamboo Rice"]
+    for u24 in range(5):
+        roll2 += ["Boston Lobster"]
+        roll2 += ["Double Scoop"]
+    for s21 in range(80):
+        roll2 += ["Tiramisu"]
+        roll2 += ["Escargot"]
+        roll2 += ["Hotdog"]
+        roll2 += ["Mango Pudding"]
+        roll2 += ["Hamburger",]
+        roll2 += ["Steak"]
+        roll2 += ["Tangyuan"]
+        roll2 += ["Sanma"]
+        roll2 += ["Napoleon Cake"]
+        roll2 += ["Salad"]
+        roll2 += ["Pastel de nata"]
+        roll2 += ["Yuxiang"]
+        roll2 += ["Sukiyaki"]
+        roll2 += ["Brownie"]
+        roll2 += ["Red Wine"]
+        roll2 += ["Gyoza"]
+        roll2 += ["Chocolate"]
+    for s22 in range(150):
+        roll2 += ["Eggette"]
+    for s23 in range(151):
+        roll2 += ["Pineapple Cake"]
+    for r21 in range(413):
+        roll2 += ["Long Bao"]
+        roll2 += ["Coffee"]
+        roll2 += ["Sashimi"]
+        roll2 += ["Macaron"]
+        roll2 += ["Zongzi"]
+        roll2 += ["Sakuramochi"]
+        roll2 += ["Tom Yum"]
+        roll2 += ["Taiyaki"]
+        roll2 += ["Milk"]
+        roll2 += ["Dorayaki"]
+        roll2 += ["Sake"]
+        roll2 += ["Tempura"]
+        roll2 += ["Spicy Gluten"]
+    for r22 in range(414):
+        roll2 += ["Jiuniang"]
+        roll2 += ["Omurice"]
+        roll2 += ["Orange Juice"]
+        roll2 += ["Ume Ochazuke" ,]
+        roll2 += ["Miso Soup" ,]
+        roll2 += ["Yellow Wine"]
+    for m21 in range(46):
+        roll2 += ["Skewer"]
+        roll2 += ["Jello"]
+        roll2 += ["Pancake"]
+    for m22 in range(47):
+        roll2 += ["Popcorn"]
+    return roll2
+
+def roll3():
+    roll3 = []
+    for u31 in range(23):
+        roll3 += ["Crab Long Bao"]
+        roll3 += ["Gingerbread"]
+    for u32 in range(61):
+        roll3 += ["Foie Gras"]
+        roll3 += ["Peking Duck"]
+        roll3 += ["B-52"]
+    for u33 in range(62):
+        roll3 += ["Bamboo Rice"]
+    for u34 in range(5):
+        roll3 += ["Boston Lobster"]
+        roll3 += ["Double Scoop"]
+    for s31 in range(76):
+        roll3 += ["Tiramisu"]
+        roll3 += ["Escargot"]
+        roll3 += ["Hotdog"]
+        roll3 += ["Mango Pudding"]
+        roll3 += ["Hamburger",]
+        roll3 += ["Steak"]
+        roll3 += ["Tangyuan"]
+        roll3 += ["Sanma"]
+        roll3 += ["Napoleon Cake"]
+        roll3 += ["Salad"]
+        roll3 += ["Pastel de nata"]
+        roll3 += ["Yuxiang"]
+        roll3 += ["Sukiyaki"]
+        roll3 += ["Brownie"]
+        roll3 += ["Red Wine"]
+        roll3 += ["Gyoza"]
+        roll3 += ["Chocolate"]
+        roll3 += ["Udon"]
+    for s32 in range(146):
+        roll3 += ["Eggette"]
+    for s33 in range(147):
+        roll3 += ["Pineapple Cake"]
+    for r31 in range(413):
+        roll3 += ["Long Bao"]
+        roll3 += ["Coffee"]
+        roll3 += ["Sashimi"]
+        roll3 += ["Macaron"]
+        roll3 += ["Zongzi"]
+        roll3 += ["Sakuramochi"]
+        roll3 += ["Tom Yum"]
+        roll3 += ["Taiyaki"]
+        roll3 += ["Milk"]
+        roll3 += ["Dorayaki"]
+        roll3 += ["Sake"]
+        roll3 += ["Tempura"]
+        roll3 += ["Spicy Gluten"]
+    for r32 in range(414):
+        roll3 += ["Jiuniang"]
+        roll3 += ["Omurice"]
+        roll3 += ["Orange Juice"]
+        roll3 += ["Ume Ochazuke" ,]
+        roll3 += ["Miso Soup" ,]
+        roll3 += ["Yellow Wine"]
+    for m31 in range(46):
+        roll3 += ["Skewer"]
+        roll3 += ["Jello"]
+        roll3 += ["Pancake"]
+    for m32 in range(47):
+        roll3 += ["Popcorn"]
+    return roll3
+
+def roll4():
+    roll4 = []
+    for u41 in range(23):
+        roll4 += ["Crab Long Bao"]
+        roll4 += ["Gingerbread"]
+    for u42 in range(61):
+        roll4 += ["Foie Gras"]
+        roll4 += ["Peking Duck"]
+        roll4 += ["B-52"]
+    for u43 in range(62):
+        roll4 += ["Bamboo Rice"]
+    for u44 in range(5):
+        roll4 += ["Boston Lobster"]
+        roll4 += ["Double Scoop"]
+    for s41 in range(72):
+        roll4 += ["Tiramisu"]
+        roll4 += ["Escargot"]
+        roll4 += ["Hotdog"]
+        roll4 += ["Mango Pudding"]
+        roll4 += ["Hamburger",]
+        roll4 += ["Steak"]
+        roll4 += ["Tangyuan"]
+        roll4 += ["Sanma"]
+        roll4 += ["Napoleon Cake"]
+        roll4 += ["Salad"]
+        roll4 += ["Pastel de nata"]
+        roll4 += ["Yuxiang"]
+        roll4 += ["Sukiyaki"]
+        roll4 += ["Brownie"]
+        roll4 += ["Red Wine"]
+        roll4 += ["Gyoza"]
+        roll4 += ["Chocolate"]
+        roll4 += ["Udon"]
+        roll4 += ["Sweet Tofu"]
+    for s42 in range(146):
+        roll4 += ["Eggette"]
+    for s43 in range(147):
+        roll4 += ["Pineapple Cake"]
+    for r41 in range(413):
+        roll4 += ["Long Bao"]
+        roll4 += ["Coffee"]
+        roll4 += ["Sashimi"]
+        roll4 += ["Macaron"]
+        roll4 += ["Zongzi"]
+        roll4 += ["Sakuramochi"]
+        roll4 += ["Tom Yum"]
+        roll4 += ["Taiyaki"]
+        roll4 += ["Milk"]
+        roll4 += ["Dorayaki"]
+        roll4 += ["Sake"]
+        roll4 += ["Tempura"]
+        roll4 += ["Spicy Gluten"]
+    for r42 in range(414):
+        roll4 += ["Jiuniang"]
+        roll4 += ["Omurice"]
+        roll4 += ["Orange Juice"]
+        roll4 += ["Ume Ochazuke" ,]
+        roll4 += ["Miso Soup" ,]
+        roll4 += ["Yellow Wine"]
+    for m41 in range(46):
+        roll4 += ["Skewer"]
+        roll4 += ["Jello"]
+        roll4 += ["Pancake"]
+    for m42 in range(47):
+        roll4 += ["Popcorn"]
+    return roll4
+
+def roll5():
+    roll5 = []
+    for u51 in range(23):
+        roll5 += ["Crab Long Bao"]
+        roll5 += ["Gingerbread"]
+    for u52 in range(61):
+        roll5 += ["Foie Gras"]
+        roll5 += ["Peking Duck"]
+        roll5 += ["B-52"]
+    for u53 in range(62):
+        roll5 += ["Bamboo Rice"]
+    for u54 in range(5):
+        roll5 += ["Boston Lobster"]
+        roll5 += ["Double Scoop"]
+    for s51 in range(72):
+        roll5 += ["Tiramisu"]
+        roll5 += ["Escargot"]
+        roll5 += ["Hotdog"]
+        roll5 += ["Mango Pudding"]
+        roll5 += ["Hamburger",]
+        roll5 += ["Steak"]
+        roll5 += ["Tangyuan"]
+        roll5 += ["Sanma"]
+        roll5 += ["Napoleon Cake"]
+        roll5 += ["Salad"]
+        roll5 += ["Pastel de nata"]
+        roll5 += ["Yuxiang"]
+        roll5 += ["Sukiyaki"]
+        roll5 += ["Brownie"]
+        roll5 += ["Red Wine"]
+        roll5 += ["Gyoza"]
+        roll5 += ["Chocolate"]
+        roll5 += ["Udon"]
+        roll5 += ["Sweet Tofu"]
+    for s52 in range(146):
+        roll5 += ["Eggette"]
+    for s53 in range(147):
+        roll5 += ["Pineapple Cake"]
+    for r51 in range(392):
+        roll5 += ["Long Bao"]
+        roll5 += ["Coffee"]
+        roll5 += ["Sashimi"]
+        roll5 += ["Macaron"]
+        roll5 += ["Zongzi"]
+        roll5 += ["Sakuramochi"]
+        roll5 += ["Cold Rice Shrimp"]
+    for r52 in range(393):
+        roll5 += ["Tom Yum"]
+        roll5 += ["Taiyaki"]
+        roll5 += ["Milk"]
+        roll5 += ["Dorayaki"]
+        roll5 += ["Sake"]
+        roll5 += ["Tempura"]
+        roll5 += ["Spicy Gluten"]
+        roll5 += ["Jiuniang"]
+        roll5 += ["Omurice"]
+        roll5 += ["Orange Juice"]
+        roll5 += ["Ume Ochazuke" ,]
+        roll5 += ["Miso Soup" ,]
+        roll5 += ["Yellow Wine"]
+    for m51 in range(46):
+        roll5 += ["Skewer"]
+        roll5 += ["Jello"]
+        roll5 += ["Pancake"]
+    for m52 in range(47):
+        roll5 += ["Popcorn"]
+    return roll5
+
+def roll6():
+    roll6 = []
+    for u61 in range(13):
+        roll6 += ["Crab Long Bao"]
+        roll6 += ["Gingerbread"]
+    for u62 in range(36):
+        roll6 += ["Foie Gras"]
+        roll6 += ["Peking Duck"]
+        roll6 += ["B-52"]
+        roll6 += ["Bamboo Rice"]
+    for u63 in range(5):
+        roll6 += ["Boston Lobster"]
+        roll6 += ["Double Scoop"]
+    for s61 in range(46):
+        roll6 += ["Tiramisu"]
+        roll6 += ["Escargot"]
+        roll6 += ["Hotdog"]
+        roll6 += ["Mango Pudding"]
+        roll6 += ["Hamburger"]
+        roll6 += ["Steak"]
+        roll6 += ["Tangyuan"]
+        roll6 += ["Sanma"]
+        roll6 += ["Napoleon Cake"]
+        roll6 += ["Salad"]
+        roll6 += ["Pastel de nata"]
+        roll6 += ["Yuxiang"]
+        roll6 += ["Sukiyaki"]
+        roll6 += ["Brownie"]
+        roll6 += ["Red Wine"]
+        roll6 += ["Gyoza"]
+        roll6 += ["Chocolate"]
+        roll6 += ["Udon"]
+        roll6 += ["Sweet Tofu"]
+        roll6 += ["Milk Tea"]
+        roll6 += ["Yunnan Noodles"]
+    for s62 in range(98):
+        roll6 += ["Eggette"]
+        roll6 += ["Pineapple Cake"]
+    for r61 in range(392):
+        roll6 += ["Long Bao"]
+        roll6 += ["Coffee"]
+        roll6 += ["Sashimi"]
+        roll6 += ["Macaron"]
+        roll6 += ["Zongzi"]
+        roll6 += ["Sakuramochi"]
+        roll6 += ["Cold Rice Shrimp"]
+    for r62 in range(393):
+        roll6 += ["Tom Yum"]
+        roll6 += ["Taiyaki"]
+        roll6 += ["Milk"]
+        roll6 += ["Dorayaki"]
+        roll6 += ["Sake"]
+        roll6 += ["Tempura"]
+        roll6 += ["Spicy Gluten"]
+        roll6 += ["Jiuniang"]
+        roll6 += ["Omurice"]
+        roll6 += ["Orange Juice"]
+        roll6 += ["Ume Ochazuke" ,]
+        roll6 += ["Miso Soup" ,]
+        roll6 += ["Yellow Wine"]
+    for m61 in range(46):
+        roll6 += ["Skewer"]
+        roll6 += ["Jello"]
+        roll6 += ["Pancake"]
+    for m62 in range(47):
+        roll6 += ["Popcorn"]
+    return roll6
+
+def roll7():
+    roll7 = []
+    for u71 in range(23):
+        roll7 += ["Crab Long Bao"]
+        roll7 += ["Gingerbread"]
+    for u72 in range(61):
+        roll7 += ["Foie Gras"]
+        roll7 += ["Peking Duck"]
+        roll7 += ["B-52"]
+    for u73 in range(62):
+        roll7 += ["Bamboo Rice"]
+    for u74 in range(5):
+        roll7 += ["Boston Lobster"]
+        roll7 += ["Double Scoop"]
+    for s71 in range(62):
+        roll7 += ["Tiramisu"]
+        roll7 += ["Escargot"]
+        roll7 += ["Hotdog"]
+        roll7 += ["Mango Pudding"]
+        roll7 += ["Hamburger",]
+        roll7 += ["Steak"]
+        roll7 += ["Tangyuan"]
+        roll7 += ["Sanma"]
+        roll7 += ["Napoleon Cake"]
+        roll7 += ["Salad"]
+        roll7 += ["Pastel de nata"]
+        roll7 += ["Yuxiang"]
+        roll7 += ["Sukiyaki"]
+        roll7 += ["Brownie"]
+        roll7 += ["Red Wine"]
+        roll7 += ["Gyoza"]
+        roll7 += ["Chocolate"]
+        roll7 += ["Udon"]
+    for s72 in range(61):
+        roll7 += ["Sweet Tofu"]
+    for s73 in range(65):
+        roll7 += ["Milk Tea"]
+        roll7 += ["Yunnan Noodles"]
+    for s74 in range(118):
+        roll7 += ["Eggette"]
+        roll7 += ["Pineapple Cake"]
+        roll7 += ["Laba Congee"]
+    for r71 in range(392):
+        roll7 += ["Long Bao"]
+        roll7 += ["Coffee"]
+        roll7 += ["Sashimi"]
+        roll7 += ["Macaron"]
+        roll7 += ["Zongzi"]
+        roll7 += ["Sakuramochi"]
+        roll7 += ["Cold Rice Shrimp"]
+    for r72 in range(393):
+        roll7 += ["Tom Yum"]
+        roll7 += ["Taiyaki"]
+        roll7 += ["Milk"]
+        roll7 += ["Dorayaki"]
+        roll7 += ["Sake"]
+        roll7 += ["Tempura"]
+        roll7 += ["Spicy Gluten"]
+        roll7 += ["Jiuniang"]
+        roll7 += ["Omurice"]
+        roll7 += ["Orange Juice"]
+        roll7 += ["Ume Ochazuke" ,]
+        roll7 += ["Miso Soup" ,]
+        roll7 += ["Yellow Wine"]
+    for m71 in range(46):
+        roll7 += ["Skewer"]
+        roll7 += ["Jello"]
+        roll7 += ["Pancake"]
+    for m72 in range(47):
+        roll7 += ["Popcorn"]
+    return roll7
+
+def roll8():
+    roll8 = []
+    for u81 in range(21):
+        roll8 += ["Crab Long Bao"]
+        roll8 += ["Gingerbread"]
+    for u82 in range(60):
+        roll8 += ["Foie Gras"]
+        roll8 += ["Peking Duck"]
+        roll8 += ["B-52"]
+        roll8 += ["Bamboo Rice"]
+    for u83 in range(9):
+        roll8 += ["Bibimbap"]
+    for u84 in range(5):
+        roll8 += ["Boston Lobster"]
+        roll8 += ["Double Scoop"]
+    for s81 in range(62):
+        roll8 += ["Tiramisu"]
+        roll8 += ["Escargot"]
+        roll8 += ["Hotdog"]
+        roll8 += ["Mango Pudding"]
+        roll8 += ["Hamburger",]
+        roll8 += ["Steak"]
+        roll8 += ["Tangyuan"]
+        roll8 += ["Sanma"]
+        roll8 += ["Napoleon Cake"]
+        roll8 += ["Salad"]
+        roll8 += ["Pastel de nata"]
+        roll8 += ["Yuxiang"]
+        roll8 += ["Sukiyaki"]
+        roll8 += ["Brownie"]
+        roll8 += ["Red Wine"]
+        roll8 += ["Gyoza"]
+        roll8 += ["Chocolate"]
+        roll8 += ["Udon"]
+    for s82 in range(61):
+        roll8 += ["Sweet Tofu"]
+    for s83 in range(65):
+        roll8 += ["Milk Tea"]
+        roll8 += ["Yunnan Noodles"]
+    for s84 in range(118):
+        roll8 += ["Eggette"]
+        roll8 += ["Pineapple Cake"]
+        roll8 += ["Laba Congee"]
+    for r81 in range(392):
+        roll8 += ["Long Bao"]
+        roll8 += ["Coffee"]
+        roll8 += ["Sashimi"]
+        roll8 += ["Macaron"]
+        roll8 += ["Zongzi"]
+        roll8 += ["Sakuramochi"]
+        roll8 += ["Cold Rice Shrimp"]
+    for r82 in range(393):
+        roll8 += ["Tom Yum"]
+        roll8 += ["Taiyaki"]
+        roll8 += ["Milk"]
+        roll8 += ["Dorayaki"]
+        roll8 += ["Sake"]
+        roll8 += ["Tempura"]
+        roll8 += ["Spicy Gluten"]
+        roll8 += ["Jiuniang"]
+        roll8 += ["Omurice"]
+        roll8 += ["Orange Juice"]
+        roll8 += ["Ume Ochazuke" ,]
+        roll8 += ["Miso Soup" ,]
+        roll8 += ["Yellow Wine"]
+    for m81 in range(46):
+        roll8 += ["Skewer"]
+        roll8 += ["Jello"]
+        roll8 += ["Pancake"]
+    for m82 in range(47):
+        roll8 += ["Popcorn"]
+    return roll8
