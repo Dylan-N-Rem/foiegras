96b96a9ca76ef910d0c7ca90532cbbed76b4b148
Dylan-N-Rem <32395209+Dylan-N-Rem@users.noreply.github.com>
2019-02-19 00:10:02 +0800

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1012,7 +1012,7 @@ async def help():
 @client.command(name = "update")
 @commands.cooldown(1, 30, commands.BucketType.user)
 async def update():
-    updatelist = [["Added summoning event: Savory Feast", "You can now summon Ddeokbokki and Kimchi from the new event!"],
+    updatelist = [["Added summoning event: Savory Feast", "You can now summon Ddeokbokki and Kimchi from the new event! After the event ends, Ddeokbokki and Kimchi will be added to the summoning pool."],
                   ["Event index list split", "The event index list has been split into two embeds due to it being too long."]]
     embed = discord.Embed(title = "Bot Update (v2.15)", description = "If there are any problems with the bot, please ping @「Pengu Pout」ディラン (Dylan) and state the problem.", color = 0x3498db)
     for updatecount in range(len(updatelist)):
